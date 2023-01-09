99cfeaf4f9271db273b293c6ad010325b5516a61
Nicolas <nicolas.gross@unimestre.com>
2023-01-09 09:46:07 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+use Laminas\Session\Container;
+use Laminas\Session\SessionManager;
+
+// Cria a sessão
+$sessionManager = new SessionManager();
+$sessionManager->start();
+
+// Cria um container de sessão com os dados fornecidos
+$session = new Container('usuario');
+$session->nome = 'Nicolas';
+$session->idade = 21;
+
+// Recupera os dados da sessão
+$nome = $session->nome;
+$idade = $session->idade;
+
+// Destrói a sessão
+$session->getManager()->destroy();
